8df932e45cb300497e2ff9a997830c85a5e260ad
rafael.carmo <rafael.carmo@CAMSTIDIS1177H.fazenda.mg>
2018-01-23 18:59:41 -0200

    Criação solution arraylist.

---
+++ hackerhank/src/hackerhank/dataStructures/Solution_arraylist.java
@@ -0,0 +1,15 @@
+package hackerhank.dataStructures;
+
+import java.util.Scanner;
+
+public class Solution_arraylist {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for (int i = 0; i < n; i++){
+			String line = sc.next();			
+			System.out.println(line);
+		}		
+	}
+}
